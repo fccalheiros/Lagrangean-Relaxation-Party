dc77354c0ab6fe9b90cc2e379ddbf3525c943d84
fccalheiros <134089857+fccalheiros@users.noreply.github.com>
2023-05-20 15:26:02 -0300

    Add files via upload

---
+++ VSFiles/Mestrado.cpp
@@ -0,0 +1,39 @@
+// Mestrado.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "ambiente/main.h"
+
+int main(int argc, char* argv[])
+{
+#ifndef NDEBUG
+    argc = 2;
+    string str0 = "C:\\Users\\fccal\\Documents\\Pessoal\\mestrado\\instancias\\w20h20n17i4.pt";
+    argv[1] = &str0[0];
+    string str1 = "";
+    argv[2] = &str1[0];
+#endif
+    try {
+        return main2(argc, argv);
+    }
+    catch (exception &e) {
+        cout << e.what() << endl;
+
+    }
+
+}
+
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
